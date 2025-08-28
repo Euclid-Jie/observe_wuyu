b3c15f8362c8ec4bb7151456a7ef64b5be4a2c5c
WeiJie Ou <ouweijie123@outlook.com>
2025-08-28 22:08:16 +0800

    fix:修正了`fetch_special_holiday`作为最小单元不从`utils`导入函数的错误

--- fetch_special_holidays.py
+++ fetch_special_holidays.py
@@ -1,8 +1,11 @@
 import pandas as pd
-from utils import connect_to_database
+import sqlalchemy
+from config import SQL_PASSWORDS, SQL_HOST
 
-engine = connect_to_database()
-query = 'SELECT date FROM Chinese_special_holiday'
+engine = sqlalchemy.create_engine(
+    f"mysql+pymysql://dev:{SQL_PASSWORDS}@{SQL_HOST}:3306/UpdatedData?charset=utf8"
+)
+query = "SELECT date FROM Chinese_special_holiday"
 holidays = pd.read_sql(query, engine)
 holidays.to_csv("Chinese_special_holiday.txt", index=False, header=False)
 print("成功更新Chinese_special_holiday.txt")
